8fd1dd2f3049615f0fd1daf6a09ee37cbfdec887
Taina <tainakroth0@gmail.com>
2025-02-26 22:04:48 -0300

    arrumei meu layout

--- poke_dex/lib/models/pokemon_response.dart
+++ poke_dex/lib/models/pokemon_response.dart
@@ -1,6 +1,5 @@
-
 import 'package:poke_dex/models/pokemon_result.dart';
-// response vai pegar as informações como a count o next o previous e os resultados para preeencher no mapa
+
 class PokemonResponse {
   final int count;
   final String? next;
@@ -8,22 +7,19 @@ class PokemonResponse {
   final List<PokemonResult> results;
 
   PokemonResponse({
-    required this.count, 
-    required this.next, 
-    required this.previous, 
+    required this.count,
+    required this.next,
+    required this.previous,
     required this.results,
-    });
+  });
 
-    factory PokemonResponse.fromMap(Map map){
-      return PokemonResponse(
-        count: map["count"], 
-        next: map["next"], 
-        previous: map["previous"], 
-        results: List<PokemonResult>.from(map["results"].map((i) => PokemonResult.fromMap(i))), 
-        
-        );
-
-    }
-    
-    
+  factory PokemonResponse.fromMap(Map map) {
+    return PokemonResponse(
+      count: map["count"],
+      next: map["next"],
+      previous: map["previous"],
+      results: List<PokemonResult>.from(
+          map["results"].map((i) => PokemonResult.fromMap(i))),
+    );
+  }
 }
